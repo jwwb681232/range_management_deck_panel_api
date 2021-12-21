2bc2d3119886973adc5b336ca1a632bfd360c65b
wangxiao <wangxiao@guadoutech.com>
2021-12-21 16:26:39 +0800

    0.5 pre

--- src/deck1_1/handler.rs
+++ src/deck1_1/handler.rs
@@ -7,7 +7,7 @@ use crate::helper::open_file;
 use crate::response::SuccessResponse;
 
 #[derive(Serialize)]
-pub struct Block1{
+pub struct AhuBstD21 {
     on: Option<String>,
     remote: Option<String>,
     trip_alarm: Option<String>,
@@ -16,10 +16,11 @@ pub struct Block1{
     air_flow_switch: Option<String>,
     supply_air_temperature: Option<String>,
     rs485_lost_com: Option<String>,
+    air_block_alarm: Option<String>,
 }
 
 #[derive(Serialize)]
-pub struct Block2{
+pub struct AhuBstD12 {
     on: Option<String>,
     remote:Option<String>,
     trip_alarm: Option<String>,
@@ -27,10 +28,11 @@ pub struct Block2{
     stop_fail_alarm: Option<String>,
     hepa_filter_alarm: Option<String>,
     air_flow_switch: Option<String>,
+    air_block_alarm: Option<String>,
 }
 
 #[derive(Serialize)]
-pub struct Block3{
+pub struct AhuBstD13 {
     on: Option<String>,
     remote: Option<String>,
     trip_alarm: Option<String>,
@@ -38,10 +40,11 @@ pub struct Block3{
     stop_fail_alarm: Option<String>,
     hepa_filter_alarm: Option<String>,
     air_flow_switch: Option<String>,
+    air_block_alarm: Option<String>,
 }
 
 #[derive(Serialize)]
-pub struct Block4{
+pub struct Common{
     co_sensor: Option<String>,
     co2_sensor: Option<String>,
     co_sensor_fault: Option<String>,
@@ -49,19 +52,11 @@ pub struct Block4{
 }
 
 #[derive(Serialize)]
-pub struct AirBlockAlarm{
-    air_block_alarm: Option<String>,
-}
-
-#[derive(Serialize)]
 pub struct Status {
-    block1:Block1,
-    block2:Block2,
-    block3:Block3,
-    block4:Block4,
-    ahu_bst_d21:AirBlockAlarm,
-    ahu_bst_d12:AirBlockAlarm,
-    ahu_bst_d13:AirBlockAlarm,
+    ahu_bst_d21:AhuBstD21,
+    ahu_bst_d12:AhuBstD12,
+    ahu_bst_d13:AhuBstD13,
+    common:Common,
 }
 
 
@@ -70,23 +65,53 @@ pub async fn index(deck: web::Data<Deck>) -> Result<impl Responder> {
     let mut contents = String::new();
 
     open_file(&deck,&deck.files.deck1_1_read,false)?.read_to_string(&mut contents).unwrap();
-    let decode_contents: Vec<String> = contents.split(";").map(|s| s.to_string()).collect();
+    let mut decode_contents: Vec<String> = contents.split(";").map(|s| s.to_string()).collect();
 
-    println!("{:?}",decode_contents.get(0));
-    println!("{:?}",decode_contents.get(29));
+    while decode_contents.len() < 29 {
+        decode_contents.push("".to_string());
+    }
 
     let success_response = SuccessResponse{
         code: StatusCode::OK.as_u16(),
         message: "success".to_string(),
-        data: Some(Block1{
-            on: decode_contents.get(29),
-            remote: None,
-            trip_alarm: None,
-            start_fail_alarm: None,
-            stop_fail_alarm: None,
-            air_flow_switch: None,
-            supply_air_temperature: None,
-            rs485_lost_com: None
+        data: Some(Status{
+            ahu_bst_d21: AhuBstD21 {
+                on: Some(decode_contents[0].to_string()),
+                remote: Some(decode_contents[1].to_string()),
+                trip_alarm: Some(decode_contents[2].to_string()),
+                start_fail_alarm: Some(decode_contents[3].to_string()),
+                stop_fail_alarm: Some(decode_contents[4].to_string()),
+                air_flow_switch: Some(decode_contents[5].to_string()),
+                supply_air_temperature: Some(decode_contents[6].to_string()),
+                rs485_lost_com: Some(decode_contents[7].to_string()),
+                air_block_alarm: Some(decode_contents[8].to_string()),
+            },
+            ahu_bst_d12: AhuBstD12 {
+                on: Some(decode_contents[9].to_string()),
+                remote: Some(decode_contents[10].to_string()),
+                trip_alarm: Some(decode_contents[11].to_string()),
+                start_fail_alarm: Some(decode_contents[12].to_string()),
+                stop_fail_alarm: Some(decode_contents[13].to_string()),
+                hepa_filter_alarm: Some(decode_contents[14].to_string()),
+                air_flow_switch: Some(decode_contents[15].to_string()),
+                air_block_alarm: Some(decode_contents[16].to_string()),
+            },
+            ahu_bst_d13: AhuBstD13 {
+                on: Some(decode_contents[17].to_string()),
+                remote: Some(decode_contents[18].to_string()),
+                trip_alarm: Some(decode_contents[19].to_string()),
+                start_fail_alarm: Some(decode_contents[20].to_string()),
+                stop_fail_alarm: Some(decode_contents[21].to_string()),
+                hepa_filter_alarm: Some(decode_contents[22].to_string()),
+                air_flow_switch: Some(decode_contents[23].to_string()),
+                air_block_alarm: Some(decode_contents[24].to_string()),
+            },
+            common: Common {
+                co_sensor: Some(decode_contents[25].to_string()),
+                co2_sensor: Some(decode_contents[26].to_string()),
+                co_sensor_fault: Some(decode_contents[27].to_string()),
+                co2_sensor_fault: Some(decode_contents[28].to_string()),
+            }
         }),
         status: "success".to_string()
     };
