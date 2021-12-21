0baf37c6cf03f099175e57140e54ca714c74f109
wangxiao <wangxiao@guadoutech.com>
2021-12-21 09:31:35 +0800

    update DeckError

--- src/error.rs
+++ src/error.rs
@@ -1,6 +1,6 @@
-use actix_web::{error, HttpResponse, http::StatusCode, dev::HttpResponseBuilder};
-use derive_more::{Display, Error};
 use crate::response::ErrorResponse;
+use actix_web::{dev::HttpResponseBuilder, error, http::StatusCode, HttpResponse};
+use derive_more::{Display, Error};
 
 #[derive(Debug, Display, Error)]
 pub enum DeckError {
@@ -10,16 +10,14 @@ pub enum DeckError {
 
 impl error::ResponseError for DeckError {
     fn error_response(&self) -> HttpResponse {
-        let return_response = ErrorResponse{
+        let return_response = ErrorResponse {
             code: 404,
             message: self.to_string(),
-            status: "error".to_string()
+            status: "error".to_string(),
         };
-
 
         HttpResponseBuilder::new(self.status_code()).json(return_response)
     }
-
 
     fn status_code(&self) -> StatusCode {
         match *self {
